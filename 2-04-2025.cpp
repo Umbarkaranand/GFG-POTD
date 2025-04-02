c7dd7516b5ca0a352a7f266cac795d6385171a1e
Anand Umbarkar <142328399+Umbarkaranand@users.noreply.github.com>
2025-04-02 17:46:30 +0530

    BFS of graph
    Given a connected undirected graph containing V vertices, represented by a 2-d adjacency list adj[][], where each adj[i] represents the list of vertices connected to vertex i. Perform a Breadth First Search (BFS) traversal starting from vertex 0, visiting vertices from left to right according to the given adjacency list, and return a list containing the BFS traversal of the graph.
    
    Note: Do traverse in the same order as they are in the given adjacency list.
    
    Examples:
    
    Input: adj[][] = [[2, 3, 1], [0], [0, 4], [0], [2]]
    
    Output: [0, 2, 3, 1, 4]
    Explanation: Starting from 0, the BFS traversal will follow these steps: 
    Visit 0 → Output: 0 
    Visit 2 (first neighbor of 0) → Output: 0, 2 
    Visit 3 (next neighbor of 0) → Output: 0, 2, 3 
    Visit 1 (next neighbor of 0) → Output: 0, 2, 3, 
    Visit 4 (neighbor of 2) → Final Output: 0, 2, 3, 1, 4
    Input: adj[][] = [[1, 2], [0, 2], [0, 1, 3, 4], [2], [2]]
    
    Output: [0, 1, 2, 3, 4]
    Explanation: Starting from 0, the BFS traversal proceeds as follows: 
    Visit 0 → Output: 0 
    Visit 1 (the first neighbor of 0) → Output: 0, 1 
    Visit 2 (the next neighbor of 0) → Output: 0, 1, 2 
    Visit 3 (the first neighbor of 2 that hasn't been visited yet) → Output: 0, 1, 2, 3 
    Visit 4 (the next neighbor of 2) → Final Output: 0, 1, 2, 3, 4

---
+++ 2-04-2025.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
+        
+        queue<int>q;
+        vector<int> res;
+        int n = adj.size();
+        vector<int> v(n+1,0);
+        v[0]=1;
+        q.push(0);
+        
+        while(!q.empty()){
+            int item = q.front();
+            q.pop();
+            res.push_back(item);
+            
+            for(auto it:adj[item]){
+                if(v[it]==0){
+                    q.push(it);
+                    v[it]=1;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        int V;
+        cin >> V;
+        cin.ignore();
+        // Use vector of vectors instead of array of vectors.
+        vector<vector<int>> adj(V);
+
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
+        }
+
+        Solution obj;
+        vector<int> ans = obj.bfs(adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
