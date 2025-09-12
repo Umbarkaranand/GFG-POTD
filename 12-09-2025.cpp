19929174d2c6db9b3351bc0bcf13f80f7908c919
Anand Umbarkar <142328399+Umbarkaranand@users.noreply.github.com>
2025-09-12 11:05:36 +0530

    Minimize the Heights II
    Given an array arr[] denoting heights of n towers and a positive integer k.
    
    For each tower, you must perform exactly one of the following operations exactly once.
    
    Increase the height of the tower by k
    Decrease the height of the tower by k
    Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
    
    You can find a slight modification of the problem here.
    Note: It is compulsory to increase or decrease the height by k for each tower. After the operation, the resultant array should not contain any negative integers.
    
    Examples :
    
    Input: k = 2, arr[] = [1, 5, 8, 10]
    Output: 5
    Explanation: The array can be modified as [1+k, 5-k, 8-k, 10-k] = [3, 3, 6, 8]. The difference between the largest and the smallest is 8-3 = 5.
    Input: k = 3, arr[] = [3, 9, 12, 16, 20]
    Output: 11
    Explanation: The array can be modified as [3+k, 9+k, 12-k, 16-k, 20-k] = [6, 12, 9, 13, 17]. The difference between the largest and the smallest is 17-6 = 11. 
    Constraints
    1 ≤ k ≤ 107
    1 ≤ n ≤ 105
    1 ≤ arr[i] ≤ 107

---
+++ 12-09-2025.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        
+        int minH=INT_MAX,maxH=INT_MIN,res=arr[n-1]-arr[0];
+        
+        for(int i=1;i<n;i++){
+            
+            if(arr[i]-k < 0)
+                continue;
+            
+            minH = min(arr[0]+k,arr[i]-k);
+            maxH = max(arr[n-1]-k,arr[i-1]+k);
+            res = min(res,maxH-minH);
+            
+        }
+        
+        return res;
+    }
+};
