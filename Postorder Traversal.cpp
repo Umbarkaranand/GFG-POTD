3462fbd3f5c4040321dd3d345e7a9350eeb705f9
Anand Umbarkar <142328399+Umbarkaranand@users.noreply.github.com>
2025-10-10 10:45:39 +0530

    09-10-2025.cpp

---
+++ Postorder Traversal.cpp
@@ -0,0 +1,33 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+    void solve(Node* root,vector<int>& ans){
+        if(root!=nullptr){
+            solve(root->left,ans);
+            solve(root->right,ans);
+            ans.push_back(root->data);
+        }
+    }
+    vector<int> postOrder(Node* root) {
+        // code here
+        
+        vector<int> ans;
+        solve(root,ans);
+        return ans;
+    }
+};
